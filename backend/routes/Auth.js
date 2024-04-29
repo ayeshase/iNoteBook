5386d455e2c98848457c6ef113cdcdb0c3518a91
unknown <ayeshazayn313@gmail.com>
2024-04-29 16:34:19 +0500

    password hashing using bcryptjs in Nodejs

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const User = require('../models/User')
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
-
-
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = "Roomiisgeniusb$oy";
 
 //Create a User using: POST "/api/auth/createuser". No Login Required 
 router.post('/createuser', [
@@ -24,19 +25,44 @@ if(user){
     return res.status(400).json({error: "Sorry a User with this email is already exists"})
 
 }
+const salt = await bcrypt.genSalt(10)
+secPass = await  bcrypt.hash(req.body.password, salt)
+
+  //Create a new user
      user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
     })
-    //.then(user=> res.json(user))
-   // .catch(err => {console.log(err)})
-   // res.json("Please Enter a Unique value for email")
-   res.json(user)
+    const data = {
+        user:{
+            id: user.id
+        }
+       }
+
+   const authtoken = jwt.sign(data, JWT_SECRET)
+   
+   
+   // res.json(user)
+    res.json(authtoken)
+
+
 }   catch( error  ){
     console.error(error.message)
     res.status(500).send("some Error occured")
 }
 
+})
+//authenticate a User using: POST "/api/auth/login". No Login Required 
+router.post('/createuser', [
+    body('email', 'Enter a valid Email' ).isEmail(),
+    body('password', 'Password cannot be blank' ).exists() ,
+
+], async (req, res) => {
+  //if there are errors, return bad request and the errors
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+  }
 })
 module.exports = router;
