da752b64e0c9926369e962840ae892d0a2c61462
unknown <ayeshazayn313@gmail.com>
2024-05-10 18:08:47 +0500

    Created Navbar And Routes in to The iNoteBook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
 
+  Route,
+
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
-    <div className='App'>
-      <h1>iNoteBook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Home />
+
+        <Route path="/">
+          <Home />
+          <Route path="/about">
+            <About />
+          </Route>
+        </Route>
+
+
+      </Router>
+    </>
   );
 }
 
